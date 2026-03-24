7f64117061421075f835601934f43a6fceadec3e
maty <maty.hattam@gmail.com>
2026-03-24 09:13:54 +0700

    tests to learn about variadic functions

--- ft_printf.c
+++ ft_printf.c
@@ -1,9 +1,30 @@
 #include "ft_printf.h"
 
-int ft_printf(const char *, ...) {}
+void parse_str(const char *s, va_list ap) {
+  // ft_putstr(va_arg(ap, char *));
+  size_t i = 0;
+  while (s[i] != '\0') {
+    ft_putchar(s[i]);
+    if (s[i] == '%') {
+      ft_putnbr(va_arg(ap, int));
+    }
+    i++;
+  }
+}
+
+int ft_printf(const char *s, ...) {
+  va_list ap;
+  int count;
+
+  va_start(ap, s);
+  parse_str(s, ap);
+  va_end(ap);
+
+  return (count);
+}
 
 int main(void) {
-  char *str = "maty est ailleurs";
+  // char *str = "maty est 24 ans";
 
-  ft_printf(str);
+  ft_printf("maty a %d ans et son père %d ans", 24, 63);
 }
