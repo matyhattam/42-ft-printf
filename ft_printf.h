3ef1b048d40d1bcea3c3302357cb2bc186cd7d91
maty <maty.hattam@gmail.com>
2026-03-24 09:13:31 +0700

    added ft_putstr

--- ft_printf.h
+++ ft_printf.h
@@ -1,7 +1,12 @@
 #ifndef FT_PRINTF_H
 #define FT_PRINTF_H
 
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
 void ft_putchar(char c);
 void ft_putnbr(int n);
+void ft_putstr(char *s);
 
 #endif
