f656f4067948fe4daab92de535886904d67186f5
maty <maty.hattam@gmail.com>
2026-03-25 11:23:04 +0700

    started parsed_specifer function

--- ft_printf.c
+++ ft_printf.c
@@ -1,10 +1,25 @@
 #include "ft_printf.h"
 
-void parse_str(const char *s, va_list ap) {
+void parse_specifier(const char *specifier, va_list ap) {
+  while (*specifier) {
+    if (ft_strchr("-+ 0#", *specifier))
+      break;
+    else if (ft_strchr("cspdiuxX%", *specifier)) {
+      ft_putnbr(va_arg(ap, int));
+      break;
+    }
+    specifier++;
+  }
+}
+
+void print_str(const char *s, va_list ap) {
   size_t i = 0;
+
   while (s[i] != '\0') {
     if (s[i] == '%') {
-      ft_putnbr(va_arg(ap, int));
+      i++;
+      parse_specifier(&s[i], ap);
+      i++;
     }
     ft_putchar(s[i]);
     i++;
@@ -16,7 +31,7 @@ int ft_printf(const char *s, ...) {
   int count;
 
   va_start(ap, s);
-  parse_str(s, ap);
+  print_str(s, ap);
   va_end(ap);
 
   return (count);
@@ -25,8 +40,10 @@ int ft_printf(const char *s, ...) {
 int main(void) {
   // char *str = "maty est 24 ans";
 
-  // ft_printf("maty a %d ans et son père %d ans", 24, 63);
-  printf("[%+10.5d] [%-10.5s] [%#010x] [% d] [%+d]\n", 42, "hello world", 255,
-         42, -42);
-  printf("[%d] [%s] [%x] [%d] [%d]\n", 42, "hello world", 255, 42, -42);
+  ft_printf("maty a %d ans et son père %d ans", 24, 63);
+  // printf("[%+10.5d] [%-10.5s] [%#010x] [% d] [%+d]\n", 42, "hello world",
+  // 255,
+  //  42, -42);
+  // printf("[%d] [%s] [%x] [%d] [%d]\n", 42, "hello world", 255, 42,
+  // -42);
 }
