b84746360872e9d716768fcf8e46fd4d978de8b8
maty <maty.hattam@gmail.com>
2026-03-25 11:22:43 +0700

    added ft_strchr

---
+++ ft_strchr.c
@@ -0,0 +1,17 @@
+#include "ft_printf.h"
+
+char *ft_strchr(const char *s, int c) {
+  size_t i = 0;
+
+  while (s[i]) {
+    if (s[i] == c) {
+      return ((char *)&s[i]);
+    }
+    i++;
+  }
+
+  if (s[i] == (char)c)
+    return ((char *)&s[i]);
+
+  return (NULL);
+}
