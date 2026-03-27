173f897b04f1d3288439cdd9ccc8e71c20ed0c30
maty <maty.hattam@gmail.com>
2026-03-27 10:41:54 +0700

    added strlen, strlcpy, strlcat, strjoin

--- ft_printf.h
+++ ft_printf.h
@@ -9,10 +9,15 @@
 
 int ft_atoi(const char *nstr);
 void *ft_calloc(size_t nmemb, size_t size);
+char *ft_itoa(int n);
 void *ft_memset(void *s, int c, size_t n);
 void ft_putchar(char c);
 void ft_putnbr(int n);
 void ft_putstr(char *s);
 char *ft_strchr(const char *s, int c);
+char *ft_strjoin(char const *s1, char const *s2);
+size_t ft_strlcat(char *dest, const char *src, size_t size);
+size_t ft_strlcpy(char *dest, const char *src, size_t size);
+size_t ft_strlen(const char *s);
 
 #endif
---
+++ ft_strlcpy.c
@@ -0,0 +1,16 @@
+#include "ft_printf.h"
+
+size_t ft_strlcpy(char *dest, const char *src, size_t size) {
+  size_t i = 0;
+
+  if (size == 0)
+    return (ft_strlen(src));
+
+  while (i < size - 1 && src[i] != '\0') {
+    dest[i] = src[i];
+    i++;
+  }
+
+  dest[i] = '\0';
+  return (ft_strlen(src));
+}
